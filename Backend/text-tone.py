21880a7b4a0c0bc4830867c2987d949609dd11a9
Arnav Garg <100040314+ArnavGarg10@users.noreply.github.com>
2025-04-26 19:13:00 -0700

    Update text-tone.py

--- Backend/text-tone.py
+++ Backend/text-tone.py
@@ -1,1 +1,14 @@
+from transformers import pipeline
 
+# Load the pre-trained Speech Emotion Recognition pipeline
+emotion_recognition = pipeline("audio-classification", model="r-f/wav2vec-english-speech-emotion-recognition")
+
+audio_path = "audio.wav"
+
+# Perform emotion recognition
+emotion = emotion_recognition(audio_path)
+
+emotion1 = emotion[0]["label"]
+emotion2 = emotion[1]["label"]
+
+print(f"The emotions you feel are: {emotion1} and {emotion2}")
